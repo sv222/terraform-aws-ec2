e00a07177ae5839b9cce4bf28abfa3a8f13eb149
Stanislav <sv_golang@mail.ru>
2023-02-23 21:41:24 +0100

    update variables

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "aws_region" {
-  description = "The AWS region to use"
-  default     = "us-east-1"
-}
-
 variable "ami_id" {
   type        = string
   description = "The ID of the Amazon Machine Image to use for the EC2 instance"
